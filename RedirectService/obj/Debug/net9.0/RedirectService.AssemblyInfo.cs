e133bacaedd169425de8762f479763c81d8031bf
quangdavids <jeejkkoo@gmail.com>
2025-04-28 20:53:21 +0700

    commit

--- RedirectService/obj/Debug/net9.0/RedirectService.AssemblyInfo.cs
+++ RedirectService/obj/Debug/net9.0/RedirectService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedirectService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ae2e019010065a7019dbb66376e59e8aee61b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedirectService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedirectService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
