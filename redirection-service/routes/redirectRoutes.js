64ef69bd13cc900343061bb37815837e828cbdd7
quangdavids <jeejkkoo@gmail.com>
2025-04-28 12:22:09 +0700

    commit

--- redirection-service/routes/redirectRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const redirectController = require('../controllers/redirectController');
-const router = express.Router();
-const { cacheMiddleware } = require('../../shorten-service/middlewares/cache');
-
-router.get('/:code', cacheMiddleware, redirectController.redirectUrl);
-
-module.exports = router;
